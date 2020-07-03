8582bf7229d574b4c52ad7f8eb53e35207f7d37f
linzheng <947169094@qq.com>
2020-07-03 15:48:11 +0800

    添加SVG图像

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import {
 } from "react-router-dom";
 import './index.scss';
 import styled from 'styled-components';
+import Nav from './components/Nav';
 
 const Wrapper = styled.div`
   min-height: 100vh;
@@ -18,21 +19,8 @@ const Main = styled.div`
   flex-grow: 1;
   overflow: auto;
 `;
-const Nav = styled.nav`
-  border:1px solid red;
-  > ul{
-  display:flex;
-    >li{
-      width: 33.3333%;
-      text-align: center;
-      padding: 16px;
-    }
-  }
-`;
 
 function App() {
-  // @ts-ignore
-  // @ts-ignore
   return (
     <Router>
       <Wrapper>
@@ -53,19 +41,7 @@ function App() {
             </Route>
           </Switch>
         </Main>
-        <Nav>
-          <ul>
-            <li>
-              <Link to="/tags">标签</Link>
-            </li>
-            <li>
-              <Link to="/money">记账</Link>
-            </li>
-            <li>
-              <Link to="/statistics">统计</Link>
-            </li>
-          </ul>
-        </Nav>
+        <Nav/>
       </Wrapper>
     </Router>
   );
