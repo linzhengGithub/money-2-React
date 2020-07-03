7507c2d27a7e97773ddb880829634ce85167f36c
linzheng <947169094@qq.com>
2020-07-03 21:00:30 +0800

    完成 Layout 和 views 组件

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,18 @@ import {
   HashRouter as Router,
   Switch,
   Route,
-  Link,
   Redirect
 } from "react-router-dom";
 import './index.scss';
-import styled from 'styled-components';
-import Nav from './components/Nav';
+import Tags from './views/Tags';
+import Money from './views/Money';
+import Statistics from './views/Statistics';
 
-const Wrapper = styled.div`
-  min-height: 100vh;
-  display:flex;
-  flex-direction: column;
-`;
-const Main = styled.div`
-  flex-grow: 1;
-  overflow: auto;
-`;
+
 
 function App() {
   return (
     <Router>
-      <Wrapper>
-        <Main>
           <Switch>
             <Route path="/tags">
               <Tags />
@@ -40,9 +30,6 @@ function App() {
               <NoMatch />
             </Route>
           </Switch>
-        </Main>
-        <Nav/>
-      </Wrapper>
     </Router>
   );
 }
@@ -52,16 +39,9 @@ function NoMatch() {
     <div>页面不存在</div>
   )
 }
-function Statistics() {
-  return <h2>统计页</h2>;
-}
 
-function Tags() {
-  return <h2>标签页</h2>;
-}
 
-function Money() {
-  return <h2>记账页</h2>;
-}
+
+
 
 export default App;
