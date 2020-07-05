280e6f9b828d17c2c71ac934eafe87b3940059d9
linzheng <947169094@qq.com>
2020-07-05 17:40:49 +0800

    完成NumberPadSection UI

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -48,7 +48,60 @@ const CategorySection = styled.section`
   
 `
 const NumberPadSection = styled.section`
+  display:flex;
+  flex-direction: column;
+  >.output{
+    background: white;
+    font-size: 36px;
+    line-height: 72px;
+    text-align: right;
+    padding: 0 16px;
+    box-shadow: inset 0 -5px 5px -5px rgba(0,0,0,0.25),
+                inset 0 5px 5px -5px rgba(0,0,0,0.25);
+  }
+  >.pad{
+    >button{
+      font-size: 18px;width: 20%;line-height:64px;border:none;
+      float: left;
+      &.ok{height: 128px;float: right;}
+      &:nth-child(1){
+        background: #f2f2f2;
+      }
+      &:nth-child(2),
+      &:nth-child(6){
+        background: #E0E0E0;
+      }
+      &:nth-child(3),
+      &:nth-child(7),
+      &:nth-child(11){
+        background: #D3D3D3;
+      }
+      &:nth-child(4),
+      &:nth-child(8),
+      &:nth-child(12),
+      &:nth-child(16){
+        background: #C1C1C1;
+      }
+      &:nth-child(5),
+      &:nth-child(9),
+      &:nth-child(13),
+      &:nth-child(17){
+        background: #B7B7B8;
+      }
+      &:nth-child(10),
+      &:nth-child(14),
+      &:nth-child(18){
+        background: #AAA8AA;
+      }
+      &:nth-child(19){
+        background: #A9A9A9;
 
+      }
+      &:nth-child(15){
+        background: #9A9A9A;
+      }
+    }
+  }
 `
 
 function Money() {
@@ -76,22 +129,27 @@ function Money() {
         </ul>
       </CategorySection>
       <NumberPadSection>
-        <div>100</div>
-        <div>
+        <div className="output">100</div>
+        <div className="pad clearfix">
           <button>1</button>
           <button>2</button>
           <button>3</button>
+          <button>+</button>
           <button>删除</button>
           <button>4</button>
           <button>5</button>
           <button>6</button>
+          <button>-</button>
           <button>清空</button>
           <button>7</button>
           <button>8</button>
           <button>9</button>
-          <button>OK</button>
+          <button>x</button>
+          <button className="ok">OK</button>
           <button>0</button>
           <button>.</button>
+          <button>%</button>
+          <button>/</button>
         </div>
       </NumberPadSection>
     </Layout>
