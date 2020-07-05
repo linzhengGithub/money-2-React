2876ef774303dc5e848561d9cf734b6b5f01d8ea
linzheng <947169094@qq.com>
2020-07-05 15:12:19 +0800

    完成NotesSection UI

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -4,30 +4,32 @@ import styled from 'styled-components';
 
 
 const TagsSection = styled.section`
-  background: white;
-  padding: 12px 16px;
+  background: white;padding: 12px 16px;
   >ol{
     margin-left: -12px;
     >li{
-      background: #D9D9D9;
-      display: inline-block;
-      margin: 8px 12px;
-      border-radius: 18px;
-      padding: 3px 18px;
-      font-size: 14px;
+      background: #D9D9D9;display: inline-block;margin: 8px 12px;
+      border-radius: 18px;padding: 3px 18px;font-size: 14px;
     }
   }
    >button{
-    background: none;
-    border: none;
-    padding: 2px 4px;
+    background: none;border: none;padding: 2px 4px;
     border-bottom: 1px solid #333;
-    color: #666;
-    margin-top: 8px;
+    color: #666;margin-top: 8px;
   }
 `
 const NotesSection = styled.section`
-
+  background: #f5f5f5;padding: 0 16px;font-size: 14px;
+  >label{
+    display: flex;align-items: center;
+    >span{
+      margin-right: 16px;white-space: nowrap;
+    }
+    >input{
+      display: block;width:100%;height: 72px;
+      background: none;border: none;
+    }
+  }
 `
 const CategorySection = styled.section`
 
@@ -51,7 +53,7 @@ function Money() {
       <NotesSection>
         <label>
           <span>备注</span>
-          <input type="text"/>
+          <input type="text" placeholder="在这里添加备注"/>
         </label>
       </NotesSection>
       <CategorySection>
