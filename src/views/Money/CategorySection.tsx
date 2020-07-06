03f9963e886ca1e28498b2fe421fddf738e608eb
linzheng <947169094@qq.com>
2020-07-06 17:05:12 +0800

    完成 NumberPadSection 的功能 wip(work in progress)

--- src/views/Money/CategorySection.tsx
+++ src/views/Money/CategorySection.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
+import React, {useState} from 'react';
 
-const CategorySection = styled.section`
+const Wrapper = styled.section`
   font-size: 24px;
   >ul{
     display: flex;background: #c4c4c4;
@@ -16,4 +17,22 @@ const CategorySection = styled.section`
   }
   
 `
+const CategorySection:React.FC = () => {
+  const [category,setCategory] = useState('-')
+  const [categoryList] = useState<('-' | '+')[]>(['-','+'])
+  const categoryMap = {'-':'支出','+':'收入'}
+  return(
+    <Wrapper>
+      <ul>
+        {categoryList.map(c =>
+          <li key={c}
+            className={category === c ? 'selected' : ''}
+              onClick={()=>{setCategory(c)}}
+          >{categoryMap[c]}</li>
+        )}
+      </ul>
+    </Wrapper>
+  )
+}
+
 export {CategorySection}
