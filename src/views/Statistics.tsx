354bd76b13e3c6433016b6c97a228db4a30592ec
linzheng <947169094@qq.com>
2020-07-10 16:12:11 +0800

    统计页 UI （wip）

--- src/views/Statistics.tsx
+++ src/views/Statistics.tsx
@@ -5,14 +5,27 @@ import styled from 'styled-components';
 import {useRecord} from '../hooks/useRecords';
 import {useTags} from '../hooks/useTags';
 import day from 'dayjs';
+
 const CategoryWrapper = styled.div`
   background: white;
-`
+`;
+const Item = styled.div`
+  display:flex;
+  justify-content: space-between;
+  background: white;
+  font-size: 18px;
+  padding: 10px 16px;
+  >.note{
+    margin-right: auto;
+    margin-left: 10px;
+    color: #999;
+  }
+`;
 
 function Statistics() {
-  const [category,setCategory] = useState<'-' | '+'>('-')
-  const {records} = useRecord()
-  const {getName} = useTags()
+  const [category, setCategory] = useState<'-' | '+'>('-');
+  const {records} = useRecord();
+  const {getName} = useTags();
   return (
     <Layout>
       <CategoryWrapper>
@@ -20,20 +33,26 @@ function Statistics() {
                          onChange={value => setCategory(value)}
         />
         <div>
-          {records.map(r =>{
-            return(
-              <div>
-                {r.tagIds.map(tagId => <span>{getName(tagId)}</span>)}
-                <hr/>
-                {r.amount}
-                <hr/>
-                {day(r.createdAt).format('YYYY年MM月DD日')}
-              </div>
-              )
+          {records.map(r => {
+            return (
+              <Item>
+                <div className="tags">
+                  {r.tagIds.map(tagId => <span>{getName(tagId)}</span>)}
+                </div>
+                {r.note && <div className="note">
+                  {r.note}
+                </div>}
+                <div className="amount">
+                  ￥{r.amount}
+                </div>
+                {/*{day(r.createdAt).format('YYYY年MM月DD日')}*/}
+              </Item>
+            );
           })}
         </div>
       </CategoryWrapper>
     </Layout>
   );
 }
-export default Statistics
+
+export default Statistics;
